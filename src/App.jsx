4b63fc7cd59acfdad86b067179b38132cb3c3b24
CodeKiro <kirofiniello@outlook.it>
2024-11-04 12:03:34 +0100

    ho implementato la parte di stats, in cui viene visualizzato il numero di items presenti nella lista, e la percentuale di completamento, in più ho utilizzato il conditional rendering per gestire se avviare il calcolo o meno

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -147,10 +147,25 @@ Item.propTypes = {
   }).isRequired,
 };
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        {" "}
+        <em>Start adding some items to your packing list</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
-    <footer>
-      <em>you have X items on your list, and you already packed X </em>
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : ` 💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
